bad9e17bca4adaccfa2ee0543bd91089c47dac42
FRANKAYFU <1647387800@qq.com>
2024-07-03 15:17:21 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+function openTab(evt, memberName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(memberName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// By default, open the first tab
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementsByClassName('tablinks')[0].click();
+});
+
+
+
